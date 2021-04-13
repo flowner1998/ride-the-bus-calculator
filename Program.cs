32ca10e4dcedcb77c9d0bc6d2b396ee43f4b10fa
Floris van Maldegem <fvmaldegem@gmail.com>
2021-04-13 12:26:56 +0200

    Added the retreiving of test values to Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace ride_the_bus_calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Read the bussize
+            int bussize;
+            Console.Write("Give the bussize: ");
+            if ( !int.TryParse(Console.ReadLine(), out bussize) ) {
+                Console.WriteLine("Didn't supply a valid integer. Exiting.");
+                return;
+            }
+            Console.WriteLine();
+
+            // read the Shuffle amount
+            int shuffleAmount;
+            Console.Write("Give the shuffle amount: ");
+            if ( !int.TryParse(Console.ReadLine(), out shuffleAmount) ) {
+                Console.WriteLine("Didn't supply a valid integer. Exiting.");
+                return;
+            }
+            Console.WriteLine();
+
+            // Read the testsize
+            int testSize;
+            Console.Write("Give the amount of tests to perform: ");
+            if ( !int.TryParse(Console.ReadLine(), out testSize) ) {
+                Console.WriteLine("Didn't supply a valid integer. Exiting.");
+                return;
+            }
+            Console.WriteLine();
+
+            
         }
     }
 }
