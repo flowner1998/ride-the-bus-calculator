7d69da807f19817d0ea88bb5203e99b853fc71df
Floris van Maldegem <fvmaldegem@gmail.com>
2021-04-13 11:53:48 +0200

    Initialized the program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ride_the_bus_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
