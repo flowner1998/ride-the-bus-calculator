c9dc8dabed0e53857b95eceb2a03b799c97ec591
Floris van Maldegem <fvmaldegem@gmail.com>
2021-04-13 17:28:17 +0200

    Added the main logic to calculate

--- Program.cs
+++ Program.cs
@@ -1,14 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace ride_the_bus_calculator
 {
     class Program
     {
+        static List<int> results;
+
         static void Main(string[] args)
         {
+            // Assign parameters for the program
             Parameters parameters = new Parameters(7, 1000, 1000);
             parameters.SetParameters();
             Console.WriteLine(parameters);
+
+            // Make a ThreadSafe random instance
+            ThreadSafeRandom rnd = new ThreadSafeRandom();
+
+            // Make a list for the results
+            results = new List<int>();
+
+            // Loop over the amount of tests
+            for (int n = 0; n < parameters.TestSize; n++)
+            {
+                // Start a thread for every test
+                Thread t = new Thread(() => Program.Calculate(rnd, parameters));
+            }
+        }
+
+
+        static void Calculate(ThreadSafeRandom random, Parameters parameters){
+            Deck deck = new Deck();
+            deck.Shuffle(random, parameters.ShuffleAmount, 0);
+
+            bool matched = false;
+            int carry = 0;
+            while( !matched)
+            {
+                // Shuffle the deck
+                deck.Shuffle(random, parameters.ShuffleAmount, parameters.BusSize);
+
+                // Get a list of the deck
+                List<Card> currentDeck = deck.GetDeck();
+
+                // Start looping through the deck. Lay down the bussize on the table and start at the next card.
+                for (int i = 0; i < currentDeck.Count - parameters.BusSize; i++)
+                {
+                    int handcard = i + parameters.BusSize;
+                    int tablecard = handcard - 1;
+                    // Check if the card in the hand matches the card on the table on the last position (we changed the order for simplicity)
+                    matched = ( currentDeck[tablecard].GetValue() == currentDeck[handcard].GetValue() );
+                    
+                    if ( matched )
+                    {
+                        // Add the amount of cards played to the result list. This is done by 
+                        results.Add( i + carry );
+                        return;
+                    }
+                }
+
+                // Couldn't find a match in this deck. Carry the amount of cards played.
+                carry = carry + currentDeck.Count - parameters.BusSize;
+
+                // Set the last table card to the new table card and swap it with the hand card.
+                Card currentLastCard = currentDeck[parameters.BusSize - 1];
+                Card newLastTableCard = currentDeck[51];
+
+                currentDeck[parameters.BusSize - 1]  = newLastTableCard;
+                currentDeck[51] = currentLastCard;
+
+                // Assign the new deck
+                deck.setCards(currentDeck);
+            }
         }
     }
 
